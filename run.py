e316f03dbab1d24b58ad13da15194051237afe34
suyan <wenqian.pi.x@gmail.com>
2024-12-11 15:15:02 +0800

    feat: add hydra

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import argparse
 import numpy as np
 from pathlib import Path
 from utils import vae_datasets
-from models import *
+from models import vae_models
 from experiment import VAEXperiment
 import pytorch_lightning as pl
 import torch.backends.cudnn as cudnn
@@ -15,54 +15,53 @@ from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
 
 from pytorch_lightning.strategies import DDPStrategy
 
-parser = argparse.ArgumentParser(description='Generic runner for VAE models')
-parser.add_argument('--config',  '-c',
-                    dest="filename",
-                    metavar='FILE',
-                    help =  'path to the config file',
-                    default='configs/sim_vae.yaml')
+import hydra
+from omegaconf import DictConfig
 
-args = parser.parse_args()
-with open(args.filename, 'r') as file:
-    try:
-        config = yaml.safe_load(file)
-    except yaml.YAMLError as exc:
-        print(exc)
+@hydra.main(version_base=None, config_path="configs", config_name="config")
+def main(config: DictConfig):
 
-save_dir = os.path.join(config['logging_params']['save_dir'], config['exp_params']['name'])
-tb_logger =  TensorBoardLogger(save_dir=save_dir,
-                               name=config['model_params']['name'],
-                               version=f"seed_{config['exp_params']['manual_seed']}")
-csv_logger = CSVLogger(save_dir=save_dir,
-                          name=config['model_params']['name'],
-                          version=f"seed_{config['exp_params']['manual_seed']}")
+    current_run_dir = hydra.utils.to_absolute_path(".")
+    tb_logger =  TensorBoardLogger(save_dir=os.getcwd(),
+                                name='',
+                                version='')
+    csv_logger = CSVLogger(save_dir=os.getcwd(),
+                            name='',
+                            version='')
+    
+    print("log dir: ", tb_logger.log_dir)
+    os.chdir(current_run_dir)
+    print("current run dir: ", current_run_dir)
+    
+    # For reproducibility
+    pl.seed_everything(config['exp_params']['manual_seed'], True)
 
-# For reproducibility
-pl.seed_everything(config['exp_params']['manual_seed'], True)
+    model = vae_models[config['model_params']['name']](**config['model_params'])
+    experiment = VAEXperiment(model,
+                            config['exp_params'])
 
-model = vae_models[config['model_params']['name']](**config['model_params'])
-experiment = VAEXperiment(model,
-                          config['exp_params'])
+    data = vae_datasets[config["data_params"]['name']](**config["data_params"], pin_memory=len(config['trainer_params']['devices']) != 0)
 
-data = vae_datasets[config["data_params"]['name']](**config["data_params"], pin_memory=len(config['trainer_params']['devices']) != 0)
+    data.setup()
 
-data.setup()
-
-runner = Trainer(logger=[tb_logger, csv_logger],
-                 callbacks=[
-                     LearningRateMonitor(),
-                     ModelCheckpoint(save_top_k=2, 
-                                     dirpath =os.path.join(tb_logger.log_dir , "checkpoints"), 
-                                     monitor= "val_loss",
-                                     save_last= True),
-                 ],
-                 strategy=DDPStrategy(find_unused_parameters=False),
-                 **config['trainer_params'])
+    runner = Trainer(logger=[tb_logger, csv_logger],
+                    callbacks=[
+                        LearningRateMonitor(),
+                        ModelCheckpoint(save_top_k=2, 
+                                        dirpath =os.path.join(tb_logger.log_dir , "checkpoints"), 
+                                        monitor= "val_loss",
+                                        save_last= True),
+                    ],
+                    # strategy=DDPStrategy(find_unused_parameters=False),
+                    **config['trainer_params'])
 
 
-Path(f"{tb_logger.log_dir}/Samples").mkdir(exist_ok=True, parents=True)
-Path(f"{tb_logger.log_dir}/Reconstructions").mkdir(exist_ok=True, parents=True)
+    Path(f"{tb_logger.log_dir}/Samples").mkdir(exist_ok=True, parents=True)
+    Path(f"{tb_logger.log_dir}/Reconstructions").mkdir(exist_ok=True, parents=True)
 
 
-print(f"======= Training {config['model_params']['name']} =======")
-runner.fit(experiment, datamodule=data)
+    print(f"======= Training {config['model_params']['name']} =======")
+    runner.fit(experiment, datamodule=data)
+    
+if __name__ == '__main__':
+    main()
